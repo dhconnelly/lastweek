8ecd6f4ddfc26c4cc18a9ffd679bd1bab035bdc0
Daniel Connelly <dhconnelly@gmail.com>
2021-04-22 14:57:59 -0400

    more fiddling for eb

--- application.py
+++ application.py
@@ -5,11 +5,11 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Snippet, Tag, tagged_snippets
 
-app = create_app(os.getenv("FLASK_CONFIG") or "default")
-migrate = Migrate(app, db)
+application = create_app(os.getenv("FLASK_CONFIG") or "default")
+migrate = Migrate(application, db)
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return dict(
         db=db,
@@ -20,17 +20,17 @@ def make_shell_context():
     )
 
 
-@app.cli.command()
+@application.cli.command()
 def fill_db():
     """Fills the dev database with fake data."""
-    if not app.config.get("DEV"):
+    if not application.config.get("DEV"):
         return
     from fill_db import fill_db
 
     fill_db(db)
 
 
-@app.cli.command()
+@application.cli.command()
 def test():
     """Run the unit tests."""
     import unittest
